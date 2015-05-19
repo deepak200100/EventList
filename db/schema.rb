d54fe8f3deded8a8167a5f2d543e0fc5b1fab98d
deepak200100 <deepak.gupta200100@gmail.com>
2015-05-20 00:43:41 +0530

    <message>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509150107) do
+ActiveRecord::Schema.define(version: 20150516063515) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "city_name",  limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "clubs", force: :cascade do |t|
     t.string   "display_name", limit: 255
