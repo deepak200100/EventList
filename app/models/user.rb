c44a329f333efaf8434b244d3ecf0c0b1f04de62
Gaurav Sachan <gaurav@freshdesk.com>
2015-05-28 02:59:44 +0530

    authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	acts_as_authentic do |c|
 	 c.validate_login_field = false
 	 c.validate_email_field = false
-	 # c.validate_password_field = false
+	 c.login_field = :email
 	end
 	validates_uniqueness_of :username,:email
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :clubs
   resources :users
+  resources :user_sessions
+  get '/logout', to: 'user_sessions#destroy', as: :logout
   root "home#index"
   
 end
